20852a7b0596abf14bff1ce87951c39e37dcf330
Jeremy Mowery <jermowery@email.arizona.edu>
2015-10-14 09:12:42 -0700

    Initial commit

---
+++ src/BirthdayListViewer.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+// TODO implement this class such that the birthdays
+// in the list are shown on the JFrame in some fashion.
+public class BirthdayListViewer extends JFrame {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
